85f4bf047c5591bf72cbe9bab1557fb1600402b2
naveen924611 <nn9331949@gmail.com>
2026-02-11 11:39:53 +0530

    completed

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,3 @@
-
-// import './App.css'
-// import Login from './pages/Login'
-
-// function App() {
-  
-//   return (
-//     <>
-//     <Login/>
-//     </>
-//   )
-// }
-
-// export default App
-
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
